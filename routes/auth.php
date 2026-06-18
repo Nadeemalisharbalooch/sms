99a55a0aa98be8840667ec8de1f5029ddbe7b83a
nadeemalisharbalooch <nadeemalisharbalooch@gmail.com>
2026-06-18 09:21:34 +0500

    add route file

--- routes/auth.php
+++ routes/auth.php
@@ -3,8 +3,10 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Institute\InstituteController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login',LoginController::class)->name('login');
 Route::post('register',RegisterController::class)->name('register');
 Route::post('logout',LogoutController::class)->name('logout')->middleware('auth');
+Route::resource('institutes',InstituteController::class);
